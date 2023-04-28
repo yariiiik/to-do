45c9a0f33ec7f06dd23ff872683ba3475ecd404d
yariiiik <vmene.e@i.ua>
2023-04-28 16:07:45 +0300

    обновление package.json

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-span = document.querySelector("#coun_letters");
+let insert_window = document.querySelector("#insert-window");
+let btn_add = document.querySelector("#btn-add");
+let bt_cancel = document.querySelector("#btn-cancel");
+
+btn_add.addEventListener('click', function(){
+    insert_window.classList.remove("d-none");
+})
+bt_cancel.addEventListener('click', function(){
+    insert_window.classList.add("d-none");
+})
+
 // document.getElementsByName("tascks")[0].value.split('\n').slice(0,3).join('\n');
-
-
